746c301abe5e125e9d131c1374b41c0f608a83f9
bmalkes <bmalkes@gmail.com>
2023-12-12 21:08:32 +0200

    fix:social

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./home.css";
 import Data from "./Data";
 import Scroll from "./ScrollDown";
+import Social from "./Social";
 
 const Home = () => {
   return (
     <section className="home section" id="home">
       <div className="home_container container grid">
         <div className="home_content grid">
-          {/* <Social /> */}
+          <Social />
           <div className="home_img"></div>
           <Data />
         </div>
