4c3b88895e1a2ab11730d7a43112066830e3d741
bmalkes <bmalkes@gmail.com>
2023-12-12 21:38:53 +0200

    fix:social

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import About from "./components/about";
 import Header from "./components/header";
 import Home from "./components/home";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Header />
       <Home />
+      <About />
     </>
   );
 }
